d4f9c43bee75e82292338fe3eb8b445326df7adc
Xu.hel <xu.hel@outlook.com>
2016-03-04 14:05:23 +0800

    移除 Admin Bar 上的 WordPress Logo

--- core.php
+++ core.php
@@ -8,13 +8,13 @@ function xuui_login_style(){
   echo "<style type=\"text/css\">@import url(\"".XUUI_PLUGIN_URL.'ui-login/login.css'."\");</style>\n";
 }
 
-/*
-//移除 Admin Bar 上的 WordPress Logo
+// Remove Admin Bar WordPress Logo
 function xuui_adminbar_remove(){
   global $wp_admin_bar;
   $wp_admin_bar->remove_menu('wp-logo');
 }
-//增加额外的联系字段
+   
+/* //增加额外的联系字段
 function xuui_user_contactmethods($user_contactmethods){
   $user_contactmethods['weibo']=__('weibo','xuui');
   $user_contactmethods['t-qq']=__('qq weibo','xuui');
