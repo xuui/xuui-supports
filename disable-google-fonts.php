f697ba74073713edbf406574a68c2af0124d93f5
Xu.hel <xu.hel@outlook.com>
2016-03-04 15:15:09 +0800

    恢复按默认主题来过滤主题的字体
    Signed-off-by: Xu.hel <xu.hel@outlook.com>

--- disable-google-fonts.php
+++ disable-google-fonts.php
@@ -1,14 +1,6 @@
 <?php //The Disable Google Fonts.
 add_filter('gettext_with_context','xuui_disable_open_sans',888,4);
-add_filter('gettext_with_context','xuui_disable_source_sans_pro',888,4);
-add_filter('gettext_with_context','xuui_disable_merriweather',888,4);
-add_filter('gettext_with_context','xuui_disable_montserrat',888,4);
-add_filter('gettext_with_context','xuui_disable_noto_sans',888,4);
-add_filter('gettext_with_context','xuui_disable_noto_serif',888,4);
-add_filter('gettext_with_context','xuui_disable_inconsolata',888,4);
-add_filter('gettext_with_context','xuui_disable_lato',888,4);
-add_filter('gettext_with_context','xuui_disable_bitter',888,4);
-
+add_action('after_setup_theme','xuui_register_theme_fonts_disabler',1);
 function xuui_disable_open_sans($translations,$text,$context,$domain){
   if('Open Sans font: on or off'==$context && 'on'==$text){$translations='off';}
   return $translations;
@@ -44,5 +36,27 @@ function xuui_disable_merriweather($translations,$text,$context,$domain){
 function xuui_disable_montserrat($translations,$text,$context,$domain){
   if('Montserrat font: on or off'==$context && 'on'==$text){$translations='off';}
   return $translations;
+}
+function xuui_register_theme_fonts_disabler(){
+  $template=get_template();
+  switch($template){
+    case 'twentysixteen' :
+      add_filter('gettext_with_context','xuui_disable_merriweather',888,4);
+      add_filter('gettext_with_context','xuui_disable_montserrat',888,4);
+      add_filter('gettext_with_context','xuui_disable_inconsolata',888,4);
+      break;
+    case 'twentyfifteen' :
+      add_filter('gettext_with_context','xuui_disable_noto_sans',888,4);
+      add_filter('gettext_with_context','xuui_disable_noto_serif',888,4);
+      add_filter('gettext_with_context','xuui_disable_inconsolata',888,4);
+      break;
+    case 'twentyfourteen' :
+      add_filter('gettext_with_context','xuui_disable_lato',888,4);
+      break;
+    case 'twentythirteen' :
+      add_filter('gettext_with_context','xuui_disable_source_sans_pro',888,4);
+      add_filter('gettext_with_context','xuui_disable_bitter',888,4);
+      break;
+  }
 }
 ?>
