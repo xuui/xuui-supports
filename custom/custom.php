7fa69ba781e8887a3f2b0acc83a087110e62b8c7
Xuhel <xu.hel@outlook.com>
2018-06-29 10:57:52 +0800

    fix css path

--- custom/custom.php
+++ custom/custom.php
@@ -6,6 +6,6 @@
 
 add_action('login_head','xuui_login_style');//自定义登录样式
 function xuui_login_style(){
-  echo "<style type=\"text/css\">@import url(\"".XUUI_PLUGIN_URL('ui-login/login.css')."\");</style>\n";
+  echo "<style type=\"text/css\">@import url(\"".XUUI_PLUGIN_URL.'custom/login.css'."\");</style>\n";
 }
 ?>
