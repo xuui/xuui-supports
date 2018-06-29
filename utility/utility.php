5e1cc1e3b02ee6f5174a472263e841e112c1d3a7
Xuhel <xu.hel@outlook.com>
2018-06-29 13:55:47 +0800

    限制分类上限为20

--- utility/utility.php
+++ utility/utility.php
@@ -6,4 +6,15 @@
 
 // Add Link Manager.
 add_filter('pre_option_link_manager_enabled','__return_true');
+
+// WordPress MU 分类上限为：20.
+add_filter('pre_insert_term',function($term,$taxonomy){
+  if($taxonomy=='product_category'){
+    if(wp_count_terms($taxonomy)>20){// max:20
+      return new WP_Error('too_much_product_category','分类上限为：20。');
+    }
+  }
+  return $term;
+},10,2);
+
 ?>
