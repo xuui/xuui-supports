02c08cb90e3dbfbf91c503bff3ae377bb0e1dec2
Xuhel <xu.hel@outlook.com>
2018-07-20 13:33:51 +0800

    color #3e588f

--- custom/custom.php
+++ custom/custom.php
@@ -9,7 +9,7 @@
 add_action('login_head','xuui_login_style',11,1);
 function xuui_login_style(){
   echo "<meta name=\"viewport\" content=\"width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=yes,shrink-to-fit=no\" />\n";
-  echo "<style type=\"text/css\">@import url(\"".XUUI_PLUGIN_URL.'custom/login.css'."\");</style>\n";
+  echo "<style type=\"text/css\">@import url(\"".XUUI_PLUGIN_URL.'custom/login2.css'."\");</style>\n";
 }
 add_action('login_form','xuui_login_script');//自定义登录表单
 add_action('lostpassword_form','xuui_login_script');//自定义找回密码表单
