a65242f36c1e41c9c6e30052c80697308c61cec8
Xuhel <xu.hel@outlook.com>
2018-08-06 13:03:16 +0800

    移除后台核心，插件和主题的更新提示.

--- utility/tuning.php
+++ utility/tuning.php
@@ -36,8 +36,14 @@ remove_filter('the_content','capital_P_dangit');
 remove_filter('the_title','capital_P_dangit');
 remove_filter('comment_text','capital_P_dangit');
 
-//移除 Shortcode 中自动添加的 br 和 p 标签.
+// 移除 Shortcode 中自动添加的 br 和 p 标签.
 remove_filter('the_content','wpautop');
 add_filter('the_content','wpautop',12);
 
+// 移除后台核心，插件和主题的更新提示.
+add_filter('pre_site_transient_update_core','__return_null');
+remove_action('load-update-core.php','wp_update_plugins');
+add_filter('pre_site_transient_update_plugins','__return_null');
+remove_action('load-update-core.php','wp_update_themes');
+add_filter('pre_site_transient_update_themes','__return_null');
 ?>
