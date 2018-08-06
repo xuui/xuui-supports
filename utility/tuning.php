29417f7c9ad673f77e0a907516ce973984be0c5b
Xuhel <xu.hel@outlook.com>
2018-08-06 12:56:17 +0800

    // 屏蔽 REST API

--- utility/tuning.php
+++ utility/tuning.php
@@ -14,12 +14,18 @@
 //add_action('wp_footer','wp_print_head_scripts',5);
 
 remove_action('wp_head','rsd_link');//XML-RPC.
-remove_action('wp_head', 'wlwmanifest_link');//Windows Live Writer.
-remove_action('wp_head', 'wp_generator');//WordPress Ver.
+remove_action('wp_head','wlwmanifest_link');//Windows Live Writer.
+remove_action('wp_head','wp_generator');//WordPress Ver.
 
-remove_action('wp_head', 'start_post_rel_link');//Post relational links.
-remove_action('wp_head', 'index_rel_link');//Post relational links.
-remove_action('wp_head', 'adjacent_posts_rel_link');//Post relational links.
+remove_action('wp_head','start_post_rel_link');//Post relational links.
+remove_action('wp_head','index_rel_link');//Post relational links.
+remove_action('wp_head','adjacent_posts_rel_link');//Post relational links.
 
+// 屏蔽 REST API
+add_filter('rest_enabled','__return_false');
+add_filter('rest_jsonp_enabled','__return_false');
+// 移除头部 wp-json 标签和 HTTP header 中的 link
+remove_action('wp_head','rest_output_link_wp_head',10);
+remove_action('template_redirect','rest_output_link_header',11);
 
 ?>
