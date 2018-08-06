f98e3cb37d835a9df4b203e751db637b651fde89
Xuhel <xu.hel@outlook.com>
2018-08-06 13:34:15 +0800

    禁用 XML-RPC 接口

--- utility/tuning.php
+++ utility/tuning.php
@@ -21,10 +21,13 @@ remove_action('wp_head','start_post_rel_link');//Post relational links.
 remove_action('wp_head','index_rel_link');//Post relational links.
 remove_action('wp_head','adjacent_posts_rel_link');//Post relational links.
 
-// 屏蔽 REST API
+// 禁用 XML-RPC 接口.
+add_filter('xmlrpc_enabled','__return_false');
+
+// 屏蔽 REST API.
 add_filter('rest_enabled','__return_false');
 add_filter('rest_jsonp_enabled','__return_false');
-// 移除头部 wp-json 标签和 HTTP header 中的 link
+// 移除头部 wp-json 标签和 HTTP header 中的 link.
 remove_action('wp_head','rest_output_link_wp_head',10);
 remove_action('template_redirect','rest_output_link_header',11);
 
@@ -37,7 +40,7 @@ remove_action('embed_head','print_emoji_detection_script');
 remove_filter('the_content_feed','wp_staticize_emoji');
 remove_filter('comment_text_rss','wp_staticize_emoji');
 remove_filter('wp_mail','wp_staticize_emoji_for_email');
-add_filter('emoji_svg_url','__return_false');//屏蔽头部加载 s.w.org
+add_filter('emoji_svg_url','__return_false');//屏蔽头部加载 s.w.org.
 
 // 移除 WordPress 自动修正 WordPress 大小写函数.
 remove_filter('the_content','capital_P_dangit');
@@ -54,23 +57,23 @@ remove_action('load-update-core.php','wp_update_plugins');
 add_filter('pre_site_transient_update_plugins','__return_null');
 remove_action('load-update-core.php','wp_update_themes');
 add_filter('pre_site_transient_update_themes','__return_null');
-add_filter('automatic_updater_disabled','__return_true');// 彻底关闭自动更新
-remove_action('init','wp_schedule_update_checks');// 关闭更新检查定时作业
-wp_clear_scheduled_hook('wp_version_check');// 移除已有的版本检查定时作业
-wp_clear_scheduled_hook('wp_update_plugins');// 移除已有的插件更新定时作业
-wp_clear_scheduled_hook('wp_update_themes');// 移除已有的主题更新定时作业
-wp_clear_scheduled_hook('wp_maybe_auto_update');// 移除已有的自动更新定时作业
-remove_action('admin_init','_maybe_update_core');// 移除后台内核更新检查
-remove_action('load-plugins.php','wp_update_plugins');// 移除后台插件更新检查
+add_filter('automatic_updater_disabled','__return_true');// 彻底关闭自动更新.
+remove_action('init','wp_schedule_update_checks');// 关闭更新检查定时作业.
+wp_clear_scheduled_hook('wp_version_check');// 移除已有的版本检查定时作业.
+wp_clear_scheduled_hook('wp_update_plugins');// 移除已有的插件更新定时作业.
+wp_clear_scheduled_hook('wp_update_themes');// 移除已有的主题更新定时作业.
+wp_clear_scheduled_hook('wp_maybe_auto_update');// 移除已有的自动更新定时作业.
+remove_action('admin_init','_maybe_update_core');// 移除后台内核更新检查.
+remove_action('load-plugins.php','wp_update_plugins');// 移除后台插件更新检查.
 remove_action('load-update.php','wp_update_plugins');
 remove_action('load-update-core.php','wp_update_plugins');
 remove_action('admin_init','_maybe_update_plugins');
-remove_action('load-themes.php','wp_update_themes');// 移除后台主题更新检查
+remove_action('load-themes.php','wp_update_themes');// 移除后台主题更新检查.
 remove_action('load-update.php','wp_update_themes');
 remove_action('load-update-core.php','wp_update_themes');
 remove_action('admin_init','_maybe_update_themes');
 
-// Disable auto-embeds for WordPress >= v3.5
+// Disable auto-embeds for WordPress >= v3.5.
 remove_filter('the_content',array($GLOBALS['wp_embed'],'autoembed'),8);
 
 // 屏蔽文章 Embed 功能.
@@ -84,9 +87,9 @@ remove_action('wp_head','wp_oembed_add_host_js');
 // 移除后台隐私相关的页面 for China.
 add_action('admin_menu',function(){
   global $menu,$submenu;
-  // 移除设置菜单下的隐私子菜单。
+  // 移除设置菜单下的隐私子菜单.
   unset($submenu['options-general.php'][45]);
-  // 移除工具彩带下的相关页面
+  // 移除工具彩带下的相关页面.
   remove_action('admin_menu','_wp_privacy_hook_requests_page');
   remove_filter('wp_privacy_personal_data_erasure_page','wp_privacy_process_personal_data_erasure_page',10,5);
   remove_filter('wp_privacy_personal_data_export_page','wp_privacy_process_personal_data_export_page',10,7);
