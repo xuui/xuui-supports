48e24a42e883e344b11228a1382269f4d6170d0e
Xuhel <xu.hel@outlook.com>
2018-08-06 13:13:03 +0800

    彻底关闭自动更新

--- utility/tuning.php
+++ utility/tuning.php
@@ -44,6 +44,21 @@ remove_action('load-update-core.php','wp_update_plugins');
 add_filter('pre_site_transient_update_plugins','__return_null');
 remove_action('load-update-core.php','wp_update_themes');
 add_filter('pre_site_transient_update_themes','__return_null');
+add_filter('automatic_updater_disabled','__return_true');// 彻底关闭自动更新
+remove_action('init','wp_schedule_update_checks');// 关闭更新检查定时作业
+wp_clear_scheduled_hook('wp_version_check');// 移除已有的版本检查定时作业
+wp_clear_scheduled_hook('wp_update_plugins');// 移除已有的插件更新定时作业
+wp_clear_scheduled_hook('wp_update_themes');// 移除已有的主题更新定时作业
+wp_clear_scheduled_hook('wp_maybe_auto_update');// 移除已有的自动更新定时作业
+remove_action('admin_init','_maybe_update_core');// 移除后台内核更新检查
+remove_action('load-plugins.php','wp_update_plugins');// 移除后台插件更新检查
+remove_action('load-update.php','wp_update_plugins');
+remove_action('load-update-core.php','wp_update_plugins');
+remove_action('admin_init','_maybe_update_plugins');
+remove_action('load-themes.php','wp_update_themes');// 移除后台主题更新检查
+remove_action('load-update.php','wp_update_themes');
+remove_action('load-update-core.php','wp_update_themes');
+remove_action('admin_init','_maybe_update_themes');
 
 // Disable auto-embeds for WordPress >= v3.5
 remove_filter('the_content',array($GLOBALS['wp_embed'],'autoembed'),8);
