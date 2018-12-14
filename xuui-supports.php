149e85fb8b42f578fa318f155abc8b25def0258c
Xuhel <xu.hel@outlook.com>
2018-12-14 11:14:19 +0800

    Version: 0.6

--- xuui-supports.php
+++ xuui-supports.php
@@ -8,7 +8,7 @@ Plugin Name: xuui Supports
 Plugin URI: http://xuui.net/plugins/xuui-supports/
 Description: This is Xu.Design WordPress Supports Plugin.
 Author: Xu.hel
-Version: 0.5.0
+Version: 0.6.0
 Author URI: http://xuui.net/
 */
 
