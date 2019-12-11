037d54d5131290d432644ad1e46164f95d11c211
Xuhel <wangxu@ccc.net>
2019-12-11 16:42:31 +0800

    正确获取文章摘要

--- utility/utility.php
+++ utility/utility.php
@@ -194,6 +194,21 @@ add_action('current_screen',function($current_screen){
 });
 */
 
+// 正确获取文章摘要: theme usr "get_post_excerpt(post-ID,255)".
+function get_post_excerpt($post,$excerpt_length=240){
+  if(!$post)$post=get_post();
+  $post_excerpt=$post->post_excerpt;
+  if($post_excerpt==''){
+    $post_content=$post->post_content;
+    $post_content=do_shortcode($post_content);
+    $post_content=wp_strip_all_tags($post_content);
+    $post_excerpt=mb_strimwidth($post_content,0,$excerpt_length,'…','utf-8');
+  }
+  $post_excerpt=wp_strip_all_tags($post_excerpt);
+  $post_excerpt=trim(preg_replace("/[\n\r\t ]+/",' ',$post_excerpt),' ');
+  return $post_excerpt;
+}
+
 /* 给管理员显示 PHP 错误. 
 if(current_user_can('manage_options')){
   define('WP_DEBUG_DISPLAY',true);
